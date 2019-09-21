52ac63497b6e2034f977392e0d800aebd38b5d37
vsRushy <gerardmarcosfreixas@gmail.com>
2019-09-21 21:35:47 +0200

    Episode 2 Roll a Ball

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float move_horizontal = Input.GetAxis("Horizontal");
+        float move_vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(move_horizontal, 0.0f, move_vertical);
+
+        rb.AddForce(movement);
+    }
+}

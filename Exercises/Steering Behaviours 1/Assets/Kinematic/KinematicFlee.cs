475e5b8b33a930ef69b0946fc5d731b2c67d2767
Gerard Marcos Freixas <gerardmf2@tr12.cat>
2019-09-27 10:28:01 +0200

    Added steering behaviours 1

---
+++ Exercises/Steering Behaviours 1/Assets/Kinematic/KinematicFlee.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicFlee : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// TODO 6: To create flee just switch the direction to go
+	}
+}
---
+++ Exercises/Steering Behaviours 1/Assets/Kinematic/KinematicSeek.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicSeek : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// TODO 1: Set movement velocity to max speed in the direction of the target
+
+        // Remember to enable this component in the inspector
+	}
+}

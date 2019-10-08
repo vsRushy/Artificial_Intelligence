88eb76480e67ffa559c2a9b1f53bc8cb42a16f44
Gerard Marcos Freixas <gerardmf2@tr12.cat>
2019-10-08 09:18:10 +0200

    Added Steering Behaviours 4 undone

---
+++ Exercises/Steering Behaviours 4/Assets/Steering/SteeringFollowPath.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+public class SteeringFollowPath : MonoBehaviour {
+
+	Move move;
+	SteeringSeek seek;
+
+    public float ratio_increment = 0.1f;
+    public float min_distance = 1.0f;
+    float current_ratio = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+		seek = GetComponent<SteeringSeek>();
+
+		// TODO 1: Calculate the closest point from the tank to the curve
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// TODO 2: Check if the tank is close enough to the desired point
+		// If so, create a new point further ahead in the path
+	}
+
+	void OnDrawGizmosSelected() 
+	{
+
+		if(isActiveAndEnabled)
+		{
+			// Display the explosion radius when selected
+			Gizmos.color = Color.green;
+			// Useful if you draw a sphere were on the closest point to the path
+		}
+
+	}
+}

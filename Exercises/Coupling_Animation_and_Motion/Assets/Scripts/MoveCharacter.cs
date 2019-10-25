cc27e21039b1607316189d7641be2f6a2cbb98c7
Gerard Marcos Freixas <gerardmf2@TR12.CAT>
2019-10-25 11:25:57 +0200

    Working correctly

---
+++ Exercises/Coupling_Animation_and_Motion/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveCharacter : MonoBehaviour
+{
+    Animator animator;
+    NavMeshAgent nav_mesh_agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        nav_mesh_agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nav_mesh_agent.remainingDistance > 0.0f)
+            animator.SetBool("move", true);
+        else
+            animator.SetBool("move", false);
+    }
+}

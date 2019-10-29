79b9a30da3da718fb43daa02c5692d2f4584b22a
Gerard Marcos Freixas <gerardmf2@TR12.CAT>
2019-10-29 09:25:38 +0100

    Trying todo 1

---
+++ Exercises/Perception_Systems/Assets/Detection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detection : MonoBehaviour
+{
+    public LayerMask mask;
+    Collider[] colliders;
+    public float radius = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliders = Physics.OverlapSphere(transform.position, radius, mask);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colliders = Physics.OverlapSphere(transform.position, radius, mask);
+        for(uint i = 0; i < colliders.Length; i++)
+        {
+            Debug.Log("Collided with " + colliders[i].gameObject);
+        }
+    }
+}

af124d633512fa044831245755f8be72a904fb04
Gerard Marcos Freixas <gerardmf2@tr12.cat>
2019-11-05 09:24:37 +0100

    Doing

---
+++ Exercises/World_Knowledge/Assets/AIKnowledgeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIKnowledgeManager : MonoBehaviour
+{
+    Dictionary<string, Knowledge> dictionary;
+
+    void Start()
+    {
+        dictionary = new Dictionary<string, Knowledge>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void AddKnowledge(string name, Knowledge knowledge)
+    {
+        Knowledge tmp_knowledge = dictionary[name];
+
+        if(tmp_knowledge == null)
+        {
+            dictionary.Add(name, new Knowledge(tmp_knowledge.game_object, tmp_knowledge.position, tmp_knowledge.time_stamp, tmp_knowledge.past_memory));
+        }
+        else
+        {
+            tmp_knowledge.Update();
+        }
+    }
+}
---
+++ Exercises/World_Knowledge/Assets/Knowledge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knowledge : MonoBehaviour
+{
+    public GameObject game_object;
+    public Vector3 position;
+    public float time_stamp;
+    public bool past_memory;
+
+    public Knowledge(GameObject game_object, Vector3 position, float time_stamp, bool past_memory)
+    {
+        this.game_object = game_object;
+        this.position = position;
+        this.time_stamp = time_stamp;
+        this.past_memory = past_memory;
+    }
+
+    public void Update()
+    {
+
+    }
+}
